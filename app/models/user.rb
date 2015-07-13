7eb0a2e198446aca681c50e041875c37207e45ab
russelltrupiano <russelltrupiano@gmail.com>
2015-07-13 15:32:57 -0400

    activation and resets active with sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
     # Accessors
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     # Filters
     before_save :downcase_email
@@ -53,6 +53,20 @@ class User < ActiveRecord::Base
       UserMailer.account_activation(self).deliver_now
     end
 
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest, User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
+
     private
 
       def downcase_email
@@ -63,4 +77,5 @@ class User < ActiveRecord::Base
         self.activation_token  = User.new_token
         self.activation_digest = User.digest(activation_token)
       end
+
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,11 +15,13 @@ class UserMailerTest < ActionMailer::TestCase
 
   test "password_reset" do
     user  = users(:russell)
+    user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Albumate password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["devs@albumate.org"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
   end
 
   test "new_album_notification" do
